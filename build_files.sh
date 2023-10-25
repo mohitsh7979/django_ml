1e9fae8f45ac63bf37abf63854075696277ef220
mohitsh7979 <mohitsh7979@gmail.com>
2023-10-25 22:56:28 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 
 echo " BUILD START"
 python3.8 -m pip install -r requirements.txt
-python 3.8 pip install django
+python3.8 pip install django
 python3.8 manage.py collectstatic --noinput --clear
 echo " BUILD END"
